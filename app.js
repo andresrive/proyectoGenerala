8fd4e0e9019641fe7d14bb4d5429cd721e5fa684
Lu <luciagomezgastaldu@gmail.com>
2023-01-30 15:32:21 +0100

    js modificado

--- app.js
+++ app.js
@@ -4,41 +4,49 @@ class Jugador {
     }
 }
 
-class Dado {
-    constructor(random) {
-        this.img
-        this.valor = random
-    }
-}
-
-class TiradadeDados{
-    constructor(){
+class Dados {
+    constructor() {
         this.dados = []
-
+        this.imgDado1 = document.createElement('img')
+        /* this.imgDado1.src = // pendiente de cargar img */
+        this.imgDado2 = document.createElement('img')
+        /*   this.imgDado1.src = // pendiente de cargar img */
+        this.imgDado3 = document.createElement('img')
+        /*   this.imgDado1.src = // pendiente de cargar img */
+        this.imgDado4 = document.createElement('img')
+        /*  this.imgDado1.src = // pendiente de cargar img */
+        this.imgDado5 = document.createElement('img')
+        /*  this.imgDado1.src = // pendiente de cargar img    */
     }
-//asigna el valor randon a los dados
-    printRandom(){
-        return  Math.floor(Math.random()*(7-1)+1)  
-     }
-    tirarDados(){
 
-        for( let i=1; i<=6; i++){
-            let dado = new Dado(this.printRandom())
+    printRandom() {
+     return (Math.floor(Math.random() * (7 - 1) + 1))
+    }
+    
+
+    tirarDados() {
+        for (let i = 1; i <= 5; i++) {
+            let dado = new Dados()
             this.dados.push(dado)
         }
     }
 
-    volverATirar(dadosATirar){
-      for(let i=0; i<dadosATirar.length; i++){
-   let indice = dadosATirar[i]
-   let dado = this.dados[indice]
-   dado.valor = this.printRandom()
-}      
+    volverATirar(dadosATirar) {
+        for (let i = 0; i < dadosATirar.length; i++) {
+            let indice = dadosATirar[i]
+            let indiceDado = this.dados[indice]
+            indiceDado.valor = this.printRandom()
+        }
+        console.log(indiceDado.valor)
     }
+
+
+
 }
 
+
 class Tablero {
-    contructor() {
+    constructor() {
         this.posibilidad1 = 0
         this.posibilidad2 = 0
         this.posibilidad3 = 0
@@ -49,53 +57,24 @@ class Tablero {
         this.full = 35
         this.pocker = 45
         this.generala = 55
-    }
-
-    //funcion que calcula las posibilidades  a nivel puntaje que tenes despues de las 3 tiradas.
-    getOptions(array){
-        let resultados = []
-        for( let i=0; i<array.length; i++){
-            let dado = array[i]
-         
-         if(!resultados[dado.valor] ){
-             let objResultado = { valor: dado.valor, cantidad:1, total:0}
-             objResultado.total = objResultado.valor * objResultado.cantidad
-             resultados[dado.valor] = objResultado
-         }
-         else{
-            let objeto = resultados[dado.valor]
-            objeto.cantidad  = objeto.cantidad+1
-            objeto.total = objeto.valor * objeto.cantidad
-            resultados[dado.valor] = objeto
-         }
-        }
-        return resultados
 
     }
+    getOptions() { }
+
 }
 
 class Juego {
-    constructor(){
-        // imagen de fondo???
-        this.tiradadeDados
-    }
-
-    start(){}
-    play(){
-        this.tiradadeDados = new TiradadeDados()
-        this.tiradadeDados.tirarDados()
-       // console.log(this.tiradadeDados)
-        console.log("-------------------")
-        this.tiradadeDados.volverATirar([1,3,5]) // indice  del array del dado q tiene q vovler a tirar
-       // console.log(this.tiradadeDados)
-        console.log("-------------------")
+    constructor() {
         this.tablero = new Tablero()
-        this.tablero.getOptions(this.tiradadeDados.dados)
-        //this.tablero.getOptions([1,3,3,5,5])
+        this.jugador = new Jugador()
+        this.dados = new Dado()
+
     }
-    end(){}
+
+    start() { }
+    play() {
+
+
+    }
+    end() { }
 }
-
-
-let juego = new Juego()
-juego.play()
